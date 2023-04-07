e33e7289d80e9217156dceb78d6ed439229105b0
FloRide1 <florian.reimat@orange.fr>
2023-04-07 12:57:59 +0200

    [DB] Add WriterModel

---
+++ src/models/writer.rs
@@ -0,0 +1,45 @@
+use std::fmt;
+
+use diesel::prelude::*;
+use serde_derive::{Deserialize, Serialize};
+
+use crate::schema::writer;
+
+#[derive(
+    Debug, PartialEq, Serialize, Deserialize, Queryable, Identifiable, Insertable, AsChangeset,
+)]
+#[diesel(table_name = writer)]
+pub struct WriterModel {
+    #[diesel(column_name = id)]
+    pub id: i32,
+
+    #[diesel(column_name = title)]
+    pub title: String,
+
+    #[diesel(column_name = name)]
+    pub name: String,
+
+    #[diesel(column_name = pseudo)]
+    pub pseudo: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Insertable)]
+#[diesel(table_name = writer)]
+pub struct NewWriter {
+    #[diesel(column_name = title)]
+    pub title: String,
+
+    #[diesel(column_name = name)]
+    pub name: String,
+
+    #[diesel(column_name = pseudo)]
+    pub pseudo: String,
+}
+
+impl fmt::Display for WriterModel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<Writer {name}>", name = self.name)
+    }
+}
+
+impl WriterModel {}
