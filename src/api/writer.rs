7a44436ae36d09aba24b1dfdf596863d94abde61
FloRide1 <florian.reimat@orange.fr>
2023-04-09 17:19:04 +0200

    [ROCKET] Add Error handling for writers endpoints

--- src/api/writer.rs
+++ src/api/writer.rs
@@ -1,4 +1,5 @@
 use diesel::{QueryDsl, RunQueryDsl};
+use rocket::http::Status;
 use rocket::{get, post, delete, patch};
 use rocket::response::status::{Created, NoContent};
 use rocket::serde::json::Json;
@@ -9,14 +10,16 @@ use crate::Db;
 
 use crate::diesel::prelude::*;
 
-
-type Result<T, E = rocket::response::Debug<diesel::result::Error>> = std::result::Result<T, E>;
-
 #[get("/")]
-pub async fn list_writers(db: Db) -> Result<Json<Vec<WriterModel>>> {
-    Ok(Json(db.run(move |conn| {
+pub async fn list_writers(db: Db) -> Result<Json<Vec<WriterModel>>, Status> {
+    let res = db.run(move |conn| {
         writer::table.load::<WriterModel>(conn)
-    }).await?))
+    }).await;
+
+    match res {
+        Ok(res) => Ok(Json(res)),
+        Err(_) => Err(Status::InternalServerError)
+    }
 }
 
 #[get("/<id>")]
@@ -26,39 +29,54 @@ pub async fn get_writer(db: Db, id: i32) -> Option<Json<WriterModel>> {
             .filter(writer::id.eq(id))
             .first(conn)
     }).await.map(Json).ok()
+
+    // TODO: Add list of circles
 }
 
 #[post("/", format = "application/json", data = "<new_writer>")]
-pub async fn new_writer(db: Db, new_writer: Json<NewWriter>) -> Result<Created<Json<WriterModel>>> {
-    let res: WriterModel = db.run(move |conn| {
+pub async fn new_writer(db: Db, new_writer: Json<NewWriter>) -> Result<Created<Json<WriterModel>>, Status> {
+    let res: Result<WriterModel, diesel::result::Error> = db.run(move |conn| {
         diesel::insert_into(writer::table)
             .values(&*new_writer)
             .get_result(conn)
-    }).await?;
+    }).await;
 
-    Ok(Created::new("/").body(Json(res)))
+    match res {
+        Ok(res) => Ok(Created::new("/").body(Json(res))),
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
 
 #[patch("/<id>", format = "application/json", data = "<new_writer>")]
-pub async fn edit_writer(db: Db, id: i32, new_writer: Json<NewWriter>) -> Result<NoContent> {
-    db.run(move |conn| {
+pub async fn edit_writer(db: Db, id: i32, new_writer: Json<NewWriter>) -> Result<NoContent, Status> {
+    let res: Result<usize, diesel::result::Error> = db.run(move |conn| {
         diesel::update(writer::table)
             .filter(writer::id.eq(id))
             .set(&*new_writer)
             .execute(conn)
-    }).await?;
+    }).await;
 
-    Ok(NoContent)
+    // TODO: Add Unauthorised ?
+    match res {
+        Ok(affected) if affected == 1 => Ok(NoContent),
+        Ok(_) => Err(Status::NotFound),
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
 
 #[delete("/<id>")]
-pub async fn delete_writer(db: Db, id: i32) -> Result<NoContent>
+pub async fn delete_writer(db: Db, id: i32) -> Result<NoContent, Status>
 {
-    db.run(move |conn| {
+    let res: Result<usize, diesel::result::Error>  = db.run(move |conn| {
         diesel::delete(writer::table)
             .filter(writer::id.eq(id))
             .execute(conn)
-    }).await?;
+    }).await;
 
-    Ok(NoContent)
+    // TODO: Add Unauthorised ?
+    match res {
+        Ok(affected) if affected == 1 => Ok(NoContent),
+        Ok(_) => Err(Status::NotFound),
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
