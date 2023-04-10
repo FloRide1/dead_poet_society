590fd8c5f7d7237296fc79be680b450dd0d67ecb
FloRide1 <florian.reimat@orange.fr>
2023-04-10 16:22:11 +0200

    [ROCKET][DB] Reformat circle Db request into CircleModel (instead of API)

--- src/api/circle.rs
+++ src/api/circle.rs
@@ -1,20 +1,15 @@
-use diesel::{QueryDsl, RunQueryDsl};
 use rocket::http::Status;
 use rocket::{get, post, delete, patch};
 use rocket::response::status::{Created, NoContent};
 use rocket::serde::json::Json;
 
 use crate::models::circle::{CircleModel, NewCircle};
-use crate::schema::circle;
 use crate::Db;
 
-use crate::diesel::prelude::*;
 
 #[get("/")]
 pub async fn list_circles(db: Db) -> Result<Json<Vec<CircleModel>>, Status> {
-    let res = db.run(move |conn| {
-        circle::table.load::<CircleModel>(conn)
-    }).await;
+    let res = CircleModel::list_circles(db).await;
 
     match res {
         Ok(res) => Ok(Json(res)),
@@ -24,22 +19,13 @@ pub async fn list_circles(db: Db) -> Result<Json<Vec<CircleModel>>, Status> {
 
 #[get("/<id>")]
 pub async fn get_circle(db: Db, id: i32) -> Option<Json<CircleModel>> {
-    db.run(move |conn| { 
-        circle::table
-            .filter(circle::id.eq(id))
-            .first(conn)
-    }).await.map(Json).ok()
-
+    CircleModel::get_circle(db, id).await.map(Json)
     // TODO: Add list of circles
 }
 
 #[post("/", format = "application/json", data = "<new_circle>")]
 pub async fn new_circle(db: Db, new_circle: Json<NewCircle>) -> Result<Created<Json<CircleModel>>, Status> {
-    let res: Result<CircleModel, diesel::result::Error> = db.run(move |conn| {
-        diesel::insert_into(circle::table)
-            .values(&*new_circle)
-            .get_result(conn)
-    }).await;
+    let res: Result<CircleModel, diesel::result::Error> = CircleModel::new_circle(db, new_circle.into_inner()).await;
 
     match res {
         Ok(res) => Ok(Created::new("/").body(Json(res))),
@@ -49,12 +35,7 @@ pub async fn new_circle(db: Db, new_circle: Json<NewCircle>) -> Result<Created<J
 
 #[patch("/<id>", format = "application/json", data = "<new_circle>")]
 pub async fn edit_circle(db: Db, id: i32, new_circle: Json<NewCircle>) -> Result<NoContent, Status> {
-    let res: Result<usize, diesel::result::Error> = db.run(move |conn| {
-        diesel::update(circle::table)
-            .filter(circle::id.eq(id))
-            .set(&*new_circle)
-            .execute(conn)
-    }).await;
+    let res: Result<usize, diesel::result::Error> = CircleModel::edit_circle(db, id, new_circle.into_inner()).await;
 
     // TODO: Add Unauthorised ?
     match res {
@@ -67,11 +48,7 @@ pub async fn edit_circle(db: Db, id: i32, new_circle: Json<NewCircle>) -> Result
 #[delete("/<id>")]
 pub async fn delete_circle(db: Db, id: i32) -> Result<NoContent, Status>
 {
-    let res: Result<usize, diesel::result::Error>  = db.run(move |conn| {
-        diesel::delete(circle::table)
-            .filter(circle::id.eq(id))
-            .execute(conn)
-    }).await;
+    let res: Result<usize, diesel::result::Error> = CircleModel::delete_circle(db, id).await;
 
     // TODO: Add Unauthorised ?
     match res {
--- src/models/circle.rs
+++ src/models/circle.rs
@@ -3,7 +3,7 @@ use std::fmt;
 use diesel::prelude::*;
 use serde_derive::{Deserialize, Serialize};
 
-use crate::schema::circle;
+use crate::{schema::circle, Db};
 
 #[derive(
     Debug, PartialEq, Serialize, Deserialize, Queryable, Identifiable, Insertable, AsChangeset,
@@ -30,4 +30,46 @@ impl fmt::Display for CircleModel {
     }
 }
 
-impl CircleModel {}
+impl CircleModel {
+    pub async fn list_circles(db: Db) -> Result<Vec<CircleModel>, diesel::result::Error>{
+        db.run(move |conn| {
+            circle::table.load::<CircleModel>(conn)
+        }).await
+
+    }
+
+    pub async fn get_circle(db: Db, id: i32) -> Option<Self> {
+        db.run(move |conn| { 
+            circle::table
+                .filter(circle::id.eq(id))
+                .first::<Self>(conn)
+        }).await.ok()
+    }
+
+    pub async fn new_circle(db: Db, new_circle: NewCircle) -> Result<CircleModel, diesel::result::Error>
+    {
+        db.run(move |conn| {
+            diesel::insert_into(circle::table)
+                .values(new_circle)
+                .get_result(conn)
+        }).await
+    }
+
+    pub async fn edit_circle(db: Db, id: i32, new_circle: NewCircle) -> Result<usize, diesel::result::Error> {
+        db.run(move |conn| {
+            diesel::update(circle::table)
+                .filter(circle::id.eq(id))
+                .set(new_circle)
+                .execute(conn)
+        }).await
+    }
+
+    pub async fn delete_circle(db: Db, id: i32) -> Result<usize, diesel::result::Error>
+    {
+        db.run(move |conn| {
+            diesel::delete(circle::table)
+                .filter(circle::id.eq(id))
+                .execute(conn)
+        }).await
+    }
+}
