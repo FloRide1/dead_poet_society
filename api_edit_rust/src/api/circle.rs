4b0a98ccd51888280e5bf7f611f3e4232e90a234
FloRide1 <florian.reimat@orange.fr>
2023-06-06 17:40:12 +0200

    [API_R][API] Remove unused path in circle.rs

--- api_edit_rust/src/api/circle.rs
+++ api_edit_rust/src/api/circle.rs
@@ -1,37 +1,11 @@
 use rocket::http::Status;
-use rocket::{get, post, delete, patch};
+use rocket::{post, delete, patch};
 use rocket::response::status::{Created, NoContent};
 use rocket::serde::json::Json;
 
 use crate::models::circle::{CircleModel, NewCircle};
 use crate::Db;
-use crate::models::writer_circle::WriterCircleModel;
 
-use super::responses::circle_response::CircleResponse;
-
-
-#[get("/")]
-pub async fn list_circles(db: Db) -> Result<Json<Vec<CircleModel>>, Status> {
-    let res = CircleModel::list_circles(&db).await;
-
-    match res {
-        Ok(res) => Ok(Json(res)),
-        Err(_) => Err(Status::InternalServerError)
-    }
-}
-
-#[get("/<id>")]
-pub async fn get_circle(db: Db, id: i32) -> Option<Json<CircleResponse>> {
-    let model = CircleModel::get_circle(&db, id).await;
-    model.as_ref()?;
-
-    let writers = WriterCircleModel::get_circle_writers(&db, id).await;
-    if writers.is_err() {
-        return Option::None;
-    }
-
-    Some(Json(CircleResponse::new(model.unwrap(), writers.unwrap_or(vec![]))))
-}
 
 #[post("/", format = "application/json", data = "<new_circle>")]
 pub async fn new_circle(db: Db, new_circle: Json<NewCircle>) -> Result<Created<Json<CircleModel>>, Status> {
